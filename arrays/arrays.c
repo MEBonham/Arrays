9f74eb73da03e37aab26865a40a1316e05494762
MEBonham <programmer@mckaybonham.com>
2019-03-25 15:28:53 -0600

    I think things are working; haven't run Tests yet.

--- arrays/arrays.c
+++ arrays/arrays.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <errno.h>
 
+
 typedef struct Array {
   int capacity;  // How many elements can this array hold?
   int count;  // How many states does the array currently hold?
@@ -29,6 +30,8 @@ Array *create_array (int capacity) {
 
   // Allocate memory for elements
   arr->elements = malloc( sizeof(char *) * capacity );
+
+  return arr;
 }
 
 
@@ -38,9 +41,13 @@ Array *create_array (int capacity) {
 void destroy_array(Array *arr) {
 
   // Free all elements
+  for (int i = 0; i < arr->count; i++)
+  {
+    free(*(arr->elements + i));
+  }
 
   // Free array
-
+  free(arr);
 }
 
 /*****
@@ -49,14 +56,25 @@ void destroy_array(Array *arr) {
  *****/
 void resize_array(Array *arr) {
 
+  // errno = EFAULT;
+  // perror("Resize not implemented yet. Array is at capacity already.");
+  // return;
+
   // Create a new element storage with double capacity
+  char **new_elements = malloc( 2 * sizeof(char *) * arr->capacity );
 
   // Copy elements into the new storage
+  for (int i = 0; i < arr->count; i++)
+  {
+    *(new_elements + i) = *(arr->elements + i);
+  }
 
   // Free the old elements array (but NOT the strings they point to)
+  free(arr->elements);
 
   // Update the elements and capacity to new values
-
+  arr->capacity *= 2;
+  arr->elements = new_elements;
 }
 
 
@@ -75,8 +93,15 @@ void resize_array(Array *arr) {
 char *arr_read(Array *arr, int index) {
 
   // Throw an error if the index is greater than the current count
+  if (index >= arr->count)
+  {
+    errno = EFAULT;
+    perror("Index out of bounds.");
+    return NULL;
+  }
 
   // Otherwise, return the element at the given index
+  return *(arr->elements + index);
 }
 
 
@@ -86,15 +111,30 @@ char *arr_read(Array *arr, int index) {
 void arr_insert(Array *arr, char *element, int index) {
 
   // Throw an error if the index is greater than the current count
+  if (index > arr->count)
+  {
+    errno = EFAULT;
+    perror("Index out of bounds.");
+    return;
+  }
 
   // Resize the array if the number of elements is over capacity
+  if (arr->count == arr->capacity)
+  {
+    resize_array(arr);
+  }
 
   // Move every element after the insert index to the right one position
+  for (int i = arr->count; i > index; i--)
+  {
+    *(arr->elements + i) = *(arr->elements + i - 1);
+  }
 
   // Copy the element and add it to the array
+  *(arr->elements + index) = strdup(element);
 
   // Increment count by 1
-
+  arr->count += 1;
 }
 
 /*****
@@ -104,11 +144,16 @@ void arr_append(Array *arr, char *element) {
 
   // Resize the array if the number of elements is over capacity
   // or throw an error if resize isn't implemented yet.
+  if (arr->count == arr->capacity)
+  {
+    resize_array(arr);
+  }
 
   // Copy the element and add it to the end of the array
+  *(arr->elements + arr->count) = strdup(element);
 
   // Increment count by 1
-
+  arr->count += 1;
 }
 
 /*****
@@ -121,11 +166,30 @@ void arr_remove(Array *arr, char *element) {
 
   // Search for the first occurence of the element and remove it.
   // Don't forget to free its memory!
+  int found = -1;
+  for (int i = 0; i < arr->count; i++)
+  {
+    if (!strcmp(element, *(arr->elements + i)))
+    {
+      found = i;
+      free(*(arr->elements + i));
+    }
+  }
+  if (found < 0)
+  {
+    errno = EDOM;
+    perror("Desired element not found in array.");
+    return;
+  }
 
   // Shift over every element after the removed element to the left one position
+  for (int i = found; i < arr->count - 1; i++)
+  {
+    *(arr->elements + i) = *(arr->elements + i + 1);
+  }
 
   // Decrement count by 1
-
+  arr->count -= 1;
 }
 
 
